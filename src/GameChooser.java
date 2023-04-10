5dbdfea3e6a5537ca9dca0aa2519c5eb7ab9656c
dougmoy <douglashtmoy@gmail.com>
2023-04-10 15:40:07 -0400

    Added game portal/chooser

---
+++ src/GameChooser.java
@@ -0,0 +1,21 @@
+public class GameChooser {
+    public static void chooseGame(){
+        System.out.println("Which game would you like to play?");
+        System.out.println("1. Monsters and Heroes");
+        System.out.println("2. Legends of Valor");
+        int getInt = Input.getInt();
+
+        while(getInt != 1 || getInt !=2){
+            System.out.println("Please enter a valid game selection");
+            getInt = Input.getInt();
+
+        }
+
+        if(getInt == 1){
+            GameStart.startGameMandH();
+        }
+        else if (getInt == 2){
+            GameStart.startGameLOV();
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,7 @@
  */
 public class Main {
     public static void main(String[] args) {
-         LegendsOfValorBoard test = new LegendsOfValorBoard(8,8);
-         test.printBoard();
-
-       //GameStartMandH.startGame();
+        GameChooser.chooseGame();
 
     }
 }
