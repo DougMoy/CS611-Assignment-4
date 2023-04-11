995a2dbe0d7017c2109fac9e35d7e1543b976789
Mark Yang <mdyang@bu.edu>
2023-04-10 20:39:01 -0400

    Additional background colors

--- src/Colors.java
+++ src/Colors.java
@@ -15,6 +15,14 @@ public class Colors {
     public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_WHITE = "\u001B[37m";
+    public static final String ANSI_BG_BLACK = "\u001B[40m";
+    public static final String ANSI_BG_RED = "\u001B[41m";
+    public static final String ANSI_BG_GREEN = "\u001B[42m";
+    public static final String ANSI_BG_YELLOW = "\u001B[43m";
+    public static final String ANSI_BG_BLUE = "\u001B[44m";
+    public static final String ANSI_BG_PURPLE = "\u001B[45m";
+    public static final String ANSI_BG_CYAN = "\u001B[46m";
+    public static final String ANSI_BG_WHITE = "\u001B[47m";
 
     public static String [] heroColors = new String[]{Colors.ANSI_BLUE,Colors.ANSI_RED, Colors.ANSI_GREEN, Colors.ANSI_YELLOW };
 }
